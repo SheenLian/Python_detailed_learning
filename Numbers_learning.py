a92ed63dcdb6f85da4f09f67b24a07b9e09069d2
SheenLian <imlianxin@gmail.com>
2017-01-20 00:16:16 +1100

    Add files via upload

---
+++ Numbers_learning.py
@@ -0,0 +1,19 @@
+import math
+import random
+#numbers
+num1 = 10  # int
+num2 = -.9876867  #float
+num3 = complex (1, 3) #complex
+num4 = math.e
+
+print(abs(num3))   #abs function can do complex as well while fabs function cant
+print('math.sqrt(10): ',math.sqrt(10))  #square root
+print('round(num2,3):  ',round(num2,3))  #round dicemal numbers
+print('math.ceil(2.3):  ',math.ceil(2.3))
+print('math.exp(2):  ',math.exp(2))
+print('math.log10(10):  ',math.log10(10))
+print('math.modf(math.pi):  ',math.modf(math.pi))
+print('choice return a random item from list, tuple or string: ')
+print("random.choice('Hello World'):  ", random.choice('Hello World'))
+
+print ('~~~~~~~~~~~~~~~~~~~~~~~~~~')
