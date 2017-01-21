61da5a526eb09af6156e71ef55113a1195b8b952
SheenLian <imlianxin@gmail.com>
2017-01-21 18:12:19 +1100

    Add files via upload

---
+++ Exceptions.py
@@ -0,0 +1,32 @@
+try:
+   fh = open("testfile", "r")
+   fh.write("This is my test file for exception handling!!")
+except IOError:
+   print ("Error: can\'t find file or read data")
+else:
+   print ("Written content in the file successfully")
+
+
+
+try:
+   fh = open("testfile", "w")
+   fh.write("This is my test file for exception handling!!")
+finally:
+   print ("Error: can\'t find file or read data")
+
+
+try:
+   fh = open("testfile", "r")
+   try:
+      fh.write("This is my test file for exception handling!!")
+   finally:
+      print ("Going to close the file")
+      fh.close()
+except IOError:
+   print ("Error: can\'t find file or read data")
+
+print('raise exception')
+def functionName( level ):
+   if level < 1:
+      raise ValueError("Invalid level!")
+functionName(0)
